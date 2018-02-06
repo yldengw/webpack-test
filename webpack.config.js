4417793be492ad83251f574d4bc250bcbbaab57e
yldengw <1130647189@qq.com>
2018-02-06 15:44:54 +0800

    less文件打包和分离以及打包路径问题

--- webpack.config.js
+++ webpack.config.js
@@ -2,9 +2,9 @@ const path = require('path');
 // const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 const htmlPlugin = require('html-webpack-plugin');
 const extractTextPlugin = require("extract-text-webpack-plugin");
-var website = {
-    publicPath: 'http://127.0.0.1:8080'
-}
+// var website = {
+//     publicPath: 'http://127.0.0.1:8080'
+// }
 module.exports = {
     // 入口文件的配置项
     entry: {
@@ -15,7 +15,7 @@ module.exports = {
     output : {
         path : path.resolve(__dirname, 'dist'),
         filename: '[name].js',
-        publicPath:website.publicPath
+        publicPath:''
     },
     module: {
         rules:[
@@ -24,10 +24,10 @@ module.exports = {
                 use: extractTextPlugin.extract({
                     fallback: "style-loader",
                     use: [{
-                        loader:'css-loader',
-                        query:{
-                            name: 'index.css'
-                        }
+                        loader:'css-loader'
+                        // query:{
+                        //     name: 'index.css'
+                        // }
                     }]
                   })
             },
@@ -35,13 +35,13 @@ module.exports = {
                 test: /\.(png|jsp|gif)/,
                 use: [{
                     loader:'file-loader',
-                    options: {
-                        limit: 500000,
-                        outputPath: 'images/'
-                    }
-                    // query:{
-                    //     name: 'assets/[hash].[ext]'
+                    // options: {
+                    //     limit: 500000,
+                    //     outputPath: '/images/'
                     // }
+                    query:{
+                        name: 'assets/[hash].[ext]'
+                    }
                 }]
             },
             {
@@ -66,7 +66,7 @@ module.exports = {
         new htmlPlugin({
             template:'./index.html'
         }),
-        new extractTextPlugin('/css/index.css')
+        new extractTextPlugin('index.css')
     ],
     devServer: {
         historyApiFallback: true,
